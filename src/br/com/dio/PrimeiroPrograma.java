17b80a7e518a956467f71caff2ad2e4723db16b0
Sergio Web <serpel@gmail.com>
2021-12-08 19:17:37 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -23,7 +23,8 @@
 // 1 - clicar na aba VCS, e depois em 'Enable Version Control Integration', selecionar Git e Ok
 // 2 - neste momento substituiu a aba VCS por git e ao abri-la, clicar na opção Commit ou ctrl+k
 // 3 - digitar a mensagem do commit, marcar todos os arquivos  e clicar em 'Commit and Push'
-// 4 -
+// 4 - definir remote e incluir a url do repositório remoto do github
+// 5 - selecionar 'Push tags' e clicar em 'Push' e fazer o login do github, em seguida autorizar o intellij
 
 package br.com.dio;
 
@@ -32,8 +33,10 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
-
         System.out.println(gato);
+
+        Livro livro1 = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro1);
 
         /*int a = 5;
         int b = 3;
@@ -41,4 +44,38 @@ public class PrimeiroPrograma {
 
     }
 
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
